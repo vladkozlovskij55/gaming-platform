7af20dfd17ce95434002ecf71916a538df332da8
Vlad <vladkozlovskij55@gmail.com>
2026-05-27 19:31:11 +0300

    fix routes imports

--- backend/server.js.js
+++ backend/server.js.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/courses", require("./routes/courses"));
-app.use("/api/results", require("./routes/results"));
+app.use("/auth", require("./routes/auth.js"));
+app.use("/courses", require("./routes/courses.js"));
+app.use("/results", require("./routes/results.js"));
 
 app.listen(5000, () => {
     console.log("Server started on port 5000");
